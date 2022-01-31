58e2dee317d68d0220beb3d640035d4ea27d4a17
Rahat Islam Srijon <52733514+rahat-srijon@users.noreply.github.com>
2022-01-31 06:52:57 +0600

    Update 11472.cpp

--- 11472.cpp
+++ 11472.cpp
@@ -5,7 +5,7 @@ int n,m,DP[10][100][1<<10];
 int bitmask_dp(int prev,int size,int mask){
 	if(size>m)return 0;
 	if(DP[prev][size][mask]!=-1)return DP[prev][size][mask];
-	int ret=mask==(1<<n)-1;
+	int ret=mask==((1<<n)-1);
 	if(prev>0)ret+=bitmask_dp(prev-1,size+1,mask|(1<<(prev-1)))%mod;
 	if(prev<n-1)ret+=bitmask_dp(prev+1,size+1,mask|(1<<(prev+1)))%mod;
 	return DP[prev][size][mask]=ret%mod;
@@ -17,8 +17,8 @@ int main(){
 		memset(DP,-1,sizeof(DP));
 		scanf("%d %d",&n,&m);
 		int ans=0,i=0;
-		while(++i<n)ans+=bitmask_dp(i,1,1<<i);
-		printf("%d\n",ans);
+		while(++i<n)ans=(ans+bitmask_dp(i,1,1<<i))%mod;
+		printf("%d\n",ans%mod);
 	}
 	return 0;
 }
