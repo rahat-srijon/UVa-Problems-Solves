3671cfff2c10582a487402d89b486675939ae517
Rahat Islam Srijon <52733514+rahat-srijon@users.noreply.github.com>
2022-02-07 04:20:17 +0600

    Update and rename 542.cpp to 542 - France '98.cpp

--- 542 - France '98.cpp
+++ 542 - France '98.cpp
@@ -24,30 +24,11 @@ int main(){
 			int c=j/s,l;
 			if(c%2==0)l=(c+1)*s;
 			else l=(c-1)*s;
-			//printf("%d: %d -- %d %d\n",s,j,l,l+s-1);
 			for(int k=l;k<l+s;k++){
 				P[j][i]+=P[j][i-1]*P[k][i-1]*W[j][k];
 			}
 		}
 	}
-	/*
-	for(int i=0;i<16;i+=2){
-		P[i][0]=W[i][i+1];
-		P[i+1][0]=W[i+1][i];
-	}
-	for(int i=0;i<16;i+=4){
-		P[i][1]=W[i][i+2]*P[i+2][0]+W[i][i+3]*P[i+3][0];
-		P[i+1][1]=W[i+1][i+2]*P[i+2][0]+W[i+1][i+3]*P[i+3][0];
-		P[i+2][1]=W[i+2][i]*P[i][0]+W[i+2][i+1]*P[i+1][0];
-		P[i+3][1]=W[i+3][i]*P[i][0]+W[i+3][i+1]*P[i+1][0];
-	}
-	for(int i=0;i<16;i+=8){
-		baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaal
-	}
-	for(int i=0;i<16;i++){
-		printf("%.2lf %.2lf\n",P[i][0],P[i][1]);
-	}
-	*/
 	for(int i=0;i<16;i++){
 		printf("%-10s p=%.2lf%%\n",Name[i],100.0*P[i][4]);
 	}
