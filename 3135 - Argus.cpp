40ec4065ba1546a0d68d643dd3a2a1ef876091b7
Rahat Islam Srijon <52733514+rahat-srijon@users.noreply.github.com>
2022-04-04 12:20:14 +0600

    Create 3135 - Argus.cpp

---
+++ 3135 - Argus.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	map<int,int>M;
+	char S[20];
+	while(1){
+		scanf("%s",S);
+		if(S[0]=='#')break;
+		int n,t;
+		scanf("%d %d",&n,&t);
+		M[n]=t;
+	}
+	int k;
+	scanf("%d",&k);
+	vector<pair<int,int> >V,P;
+	for(auto [x,y]:M){
+		for(int i=1;i<=k;i++){
+			V.push_back({i*y,x});
+		}
+	}
+	sort(V.begin(),V.end());
+	for(int i=0;i<k;i++){
+		printf("%d\n",V[i].second);
+	}
+	return 0;
+}
